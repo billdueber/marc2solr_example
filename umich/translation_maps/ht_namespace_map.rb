798095ec48ce73c34a669703f3817c66816ed904
BillDueber <bill@dueber.com>
2010-09-16 12:56:51 -0400

    Fixed namespace for ht

--- umich/translation_maps/ht_namespace_map.rb
+++ umich/translation_maps/ht_namespace_map.rb
@@ -1,17 +1,24 @@
 {
  :maptype=>:kv,
  :mapname=>"ht_namespace_map",
- :map => {"mdp"=>"University of Michigan",
- "gwla"=>"University of Michigan",
- "pst"=>"Penn State University",
- "miua"=>"University of Michigan",
- "inu"=>"Indiana University",
- "wu"=>"University of Wisconsin",
- "nnc2"=>"Columbia University",
- "nnc1"=>"Columbia University",
- "umn"=>"University of Minnesota",
- "uc2"=>"University of California",
- "uc1"=>"University of California",
- "miun"=>"University of Michigan"}
+ :map => {
+   "gwla" => "University of Michigan",
+   "inu" => "Indiana University",
+   "mdp" => "University of Michigan",
+   "miua" => "University of Michigan",
+   "miun" => "University of Michigan",
+   "njp" => "Princeton University",
+   "nnc1" => "Columbia University",
+   "nnc2" => "Columbia University",
+   "nyp" => "New York Public Library",
+   "pst" => "Penn State University",
+   "uc1" => "University of California",
+   "uc2" => "University of California",
+   "uiuo" => "University of Illinois",
+   "umn" => "University of Minnesota",
+   "wu" => "University of Wisconsin",
+   "yale" => "Yale University"
+ }
 
 }
+
