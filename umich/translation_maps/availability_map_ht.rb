3823ac1e9436b8edcdf7fb65ccc48eb469ef743f
billdueber <bill@dueber.com>
2011-03-08 10:44:57 -0500

    Changed availbility_map_ht* to be multi maps and use cc* for CC licenses

--- umich/translation_maps/availability_map_ht.rb
+++ umich/translation_maps/availability_map_ht.rb
@@ -1,14 +1,19 @@
 {
- :maptype=>:kv,
- :mapname=>"availability_map_ht",
- :map => {"ic"=>"Search only",
- "umall"=>"Full text",
- "orph"=>"Search only",
- "world"=>"Full text",
- "nobody"=>"Search only",
- "pd"=>"Full text",
- "pdus"=>"Full text",
- "und"=>"Search only",
- "opb"=>"Full text"}
+   :maptype=>:multi,
+   :mapname=>"availability_map_ht",
+   :map => [
+      [/^ic$/, "Search only"],
+      [/^umall$/, "Full text"],
+      [/^orph$/, "Search only"],
+      [/^world$/, "Full text"],
+      [/^nobody$/, "Search only"],
+      [/^und$/, "Search only"],
+      [/^opb$/, "Full text"],
 
+      [/^cc.*/, "Full text"],
+
+      [/^pd.*/, "Full text"],
+
+
+   ]
 }
--- umich/translation_maps/availability_map_ht_intl.rb
+++ umich/translation_maps/availability_map_ht_intl.rb
@@ -1,14 +1,20 @@
 {
- :maptype=>:kv,
- :mapname=>"availability_map_ht_intl",
- :map => {"ic"=>"Search only",
- "umall"=>"Search only",
- "orph"=>"Search only",
- "world"=>"Full text",
- "nobody"=>"Search only",
- "pd"=>"Full text",
- "pdus"=>"Search only",
- "und"=>"Search only",
- "opb"=>"Full text"}
+   :maptype=>:multi,
+   :mapname=>"availability_map_ht_intl",
+   :map => [
+      [/^ic$/, "Search only"],
+      [/^umall$/, "Full text"],
+      [/^orph$/, "Search only"],
+      [/^world$/, "Full text"],
+      [/^nobody$/, "Search only"],
+      [/^und$/, "Search only"],
+      [/^opb$/, "Full text"],
 
+      [/^cc.*/, "Full text"],
+
+      [/^pd$/, "Full text"],
+      [/^pdus$/, "Search only"],
+
+
+   ]
 }
