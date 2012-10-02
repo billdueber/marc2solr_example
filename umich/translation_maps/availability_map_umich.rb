7f04b1a823d640b894fd0875d4aace53ef064000
billdueber <bill@dueber.com>
2012-10-02 10:26:14 -0400

    Added avail_ebm_pod to map

--- umich/translation_maps/availability_map_umich.rb
+++ umich/translation_maps/availability_map_umich.rb
@@ -1,11 +1,15 @@
 {
  :maptype=>:kv,
  :mapname=>"availability_map_umich",
- :map => {"avail_ht_fulltext"=>"Full text available online via HathiTrust",
- "avail_circ"=>"Circulating volumes",
- "avail_checkout"=>"Available to check out",
- "avail_online"=>"Available online",
- "avail_recall"=>"Checked out (Available for recall)",
- "avail_ht"=>"HathiTrust"}
+ :map => {
+   "avail_ht_fulltext"=>"Full text available online via HathiTrust",
+   "avail_circ"=>"Circulating volumes",
+   "avail_checkout"=>"Available to check out",
+   "avail_online"=>"Available online",
+   "avail_recall"=>"Checked out (Available for recall)",
+   "avail_ht"=>"HathiTrust",
+   "avail_ebm_pod" => "Available for Print on Demand"
+
+ }
 
 }
